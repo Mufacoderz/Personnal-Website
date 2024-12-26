30f24b94f607e18db699bae3334c5b065ac6f4ee
Muhammad <mf9555759@gmail.com>
2024-12-26 09:29:34 +0800

    menginstall tailwind css, dan menambahkan node modules ke gitignore

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.html",
+    "./public/css/**/*.css",
+    "./src/**/*.{js,css}", 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
