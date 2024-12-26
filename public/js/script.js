9aec48493af1960852582ab42e0368ab6a629f9b
Muhammad <mf9555759@gmail.com>
2024-12-26 10:08:21 +0800

    menyimpan sebelum switch ke main

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,10 @@
-// Function to handle redirect
+
 function openMainPage() {
     document.body.classList.add('fade-out');
     setTimeout(() => {
-        // Redirect to main page in the same tab
         window.location.href = "main.html";
     }, 1000);
 }
 
-// Add event listener to the button
+
 document.getElementById('start-button').addEventListener('click', openMainPage);
